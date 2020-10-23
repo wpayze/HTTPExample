6585d7ba2329f32ddfe6f5fbcfe1a8e89a308ba9
Wilfredo <wilfredopaizpaz@gmail.com>
2020-10-22 23:10:17 -0600

    cambios de segundo dia

--- src/components/create.vue
+++ src/components/create.vue
@@ -1,5 +1,14 @@
 <template>
   <v-container>
+
+    <div class="label-success" v-if="estado == true">
+      TODO FUE UN EXITO FELICIDADES
+    </div>
+
+    <div class="label-error" v-if="estado == false">
+      ESTO NO SIRVIO NI PIJA
+    </div>
+
     <h1>NUEVO POST</h1>
 
     <v-text-field v-model="title" label="TITULO" required></v-text-field>
@@ -17,21 +26,46 @@ export default {
     return {
       title: "",
       body: "",
+      estado: null
     };
   },
   methods: {
       async guardar() {
+
         const { data: response } = await axios.post("https://jsonplaceholder.typicode.com/posts", 
         {
             title: this.title,
             body: this.body
         });
 
-        console.log(response);
+        if ( !response.id ) {
+          this.$toast.success("FELICIDADES !!!!");
+          //this.$router.push("/");
+          this.estado = true;
+        } else {
+          this.$toast.error("ESTO NO SIRVIO");
+          this.estado = false;
+        }
+
+        var instancia = this;
+        setTimeout( () => {
+          instancia.estado = null;
+        }, 2000);
       }
   }
 };
 </script>
 
 <style>
+  .label-success {
+    background: green;
+    color: #fff;
+    padding: 1em;
+  }
+
+  .label-error {
+    background: red;
+    color: #fff;
+    padding: 1em;
+  }
 </style>
